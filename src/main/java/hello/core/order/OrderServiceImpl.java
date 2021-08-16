b0480cfe1a82a97f6cf3f1d316058f6e948f0c95
Hott-J <jhj13062004@naver.com>
2021-08-17 01:37:02 +0900

    fixed

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,16 +9,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderServiceImpl implements OrderService{
 
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository; // final 쓰면 생성자에서만 값을 넣어줄 수 있음!!!
+    // 생성자에서 memberRepository를 누락한 것도 알 수 있게 자바 컴파일에서 오류메시지를 알려준다
     private final DiscountPolicy discountPolicy;
 
 //    @Autowired -> 필드 주입
 //    private MemberRepository memberRepository;
 
-    @Autowired(required = false) // 선택적으로 동작 -> 수정자 주입
-    public void setMemberRepository(MemberRepository memberRepository){
-        this.memberRepository = memberRepository;
-    }
+//    @Autowired(required = false) // 선택적으로 동작 -> 수정자 주입
+//    public void setMemberRepository(MemberRepository memberRepository){
+//        this.memberRepository = memberRepository;
+//    }
 
     //@Autowired 생성자가 1개면 생략 가능 -> 생성자 주입6
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
