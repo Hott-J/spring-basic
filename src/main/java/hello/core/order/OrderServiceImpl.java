1be3f034114203e89fbc1922df61b4dbc1108288
Hott-J <jhj13062004@naver.com>
2021-09-01 01:16:22 +0900

    fixed

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Service
+//@RequiredArgsConstructor // final이 붙은애를 인자로 생성자 만들어줌
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // final 쓰면 생성자에서만 값을 넣어줄 수 있음!!!
@@ -21,8 +26,8 @@ public class OrderServiceImpl implements OrderService{
 //        this.memberRepository = memberRepository;
 //    }
 
-    //@Autowired 생성자가 1개면 생략 가능 -> 생성자 주입6
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired //생성자가 1개면 생략 가능 -> 생성자 주입, bean이름을 반환타입이 아닌 필드명까지해서 이름 명명해줌. 똑같은 타입 여러개 빈을 구분해줌
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
